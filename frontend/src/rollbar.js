954d4b584a6e817fb754432536eb9807d075ec46
zalyalova <zalyalova1962@icloud.com>
2026-06-05 12:46:30 +0300

    fix

--- frontend/src/rollbar.js
+++ frontend/src/rollbar.js
@@ -1,10 +1,13 @@
 import Rollbar from 'rollbar';
 
+const accessToken = import.meta.env.VITE_ROLLBAR_TOKEN;
+
 const rollbar = new Rollbar({
-  accessToken: import.meta.env.VITE_ROLLBAR_TOKEN,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-  environment: 'development',
+  accessToken: accessToken || 'empty',
+  enabled: Boolean(accessToken),
+  captureUncaught: Boolean(accessToken),
+  captureUnhandledRejections: Boolean(accessToken),
+  environment: import.meta.env.MODE,
 });
 
 export default rollbar;
